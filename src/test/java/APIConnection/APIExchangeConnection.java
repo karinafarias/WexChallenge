0e018f5a4c49794d9e446a079d9cf36599e99575
karinaxavier <karinaxavier@NBRJ027795.BRQ.COM>
2021-07-28 12:59:16 -0300

    APIExchance.java with error in line 27.

--- src/test/java/APIConnection/APIExchangeConnection.java
+++ src/test/java/APIConnection/APIExchangeConnection.java
@@ -17,19 +17,19 @@ import paginas.PageAmazon;
 import utils.RegrasUtils;
 
 public class APIExchangeConnection {
-	Logger logger = LogWeb.getLogger(APIExchangeConnection.class);
-	// Setting URL
-	String url_str ="http://api.exchangeratesapi.io/v1/convert?access_key=1046569ff7463244cec3583a5f2fd944&from=USD&to=EUR& amount = 25";
-
-	// Making Request
-	URL url = new URL(url_str);
-	HttpURLConnection request = (HttpURLConnection) url.openConnection();
-	request.connect();
-
-	// Convert to JSON
-	JsonParser jp = new JsonParser();
-	JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-	JsonObject jsonobj = root.getAsJsonObject();
+//	Logger logger = LogWeb.getLogger(APIExchangeConnection.class);
+//	// Setting URL
+//	String url_str ="http://api.exchangeratesapi.io/v1/convert?access_key=1046569ff7463244cec3583a5f2fd944&from=USD&to=EUR& amount = 25";
+//
+//	// Making Request
+//	URL url = new URL(url_str);
+//	HttpURLConnection request = (HttpURLConnection) url.openConnection();
+//	request.connect();
+//
+//	// Convert to JSON
+//	JsonParser jp = new JsonParser();
+//	JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+//	JsonObject jsonobj = root.getAsJsonObject();
 
 	// Accessing object
 //	String req_result = jsonobj.get("result").getAsString();
