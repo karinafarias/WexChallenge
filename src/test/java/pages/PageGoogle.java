5e449e8b70704c24c0c52033a89de8374681d41d
karinaxavier <karinaxavier@host.docker.internal>
2021-07-29 08:24:31 -0300

    more annotations

--- src/test/java/pages/PageGoogle.java
+++ src/test/java/pages/PageGoogle.java
@@ -2,10 +2,18 @@ package pages;
 
 import elements.ElementsGoogle;
 import interation.InteractionWeb;
-
+/**
+ * This class contains only methods about search in Google page
+ */
 public class PageGoogle implements InteractionWeb {
 	private ElementsGoogle eleGoogle = new ElementsGoogle();
 	
+	/**
+	 * This method uses the url as parameter, before this this method checks if the url has 
+	 * http or https and put it if necessary. It opens the browser with this url.
+	 * 
+	 * 
+	 */
 	public void openGoogle(String url) {
 		if(!url.matches("[http{s}://]+")) {
 			url="https://"+url;
@@ -13,12 +21,17 @@ public class PageGoogle implements InteractionWeb {
 		openUrl(url);
 		waitUrlLoading(url, 20);
 	}
-
+	/**
+	 * This method writes some text in search bar on Google 
+	 */
 	public void searchFor(String text) {
 		write(eleGoogle.getInputSearchBarGoogle(), text);
 		click(eleGoogle.getBtnSearchGoogle());
 	}
 
+	/**
+	 * This method looking for Amazon url and click it on Google search page 
+	 */
 	public void navigateTo(String url) {
 		if(!url.matches("[http{s}://]+")) {
 			url="https://"+url+"/";
